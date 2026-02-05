8e04c160b8c59e1a1d8c6f1b1d513efb743e6bb3
Smaran Vangala <vangalasmaran@gmail.com>
2026-02-04 21:36:04 -0800

    Update SQL_BASICS.sql

--- SQL_BASICS.sql
+++ SQL_BASICS.sql
@@ -37,29 +37,115 @@ SELECT COUNT(CustomerID)
 FROM orders;
 
 -- Q9: Find countries where the average unit price is greater than 5.
+SELECT Country, ROUND(AVG(UnitPrice),2)
+FROM orders
+GROUP BY Country HAVING AVG(UnitPrice) > 5;
 
 -- Q10: Retrieve rows where Description starts with 'WHITE'.
+SELECT Description
+FROM orders
+WHERE Description LIKE 'WHITE%';
+
 -- Q11: Find transactions where Quantity is negative.
+SELECT InvoiceNo, Quantity
+FROM orders
+WHERE Quantity < 0;
+
 -- Q12: Show the first 20 rows of the table.
+SELECT *
+FROM orders
+LIMIT 20;
+
 -- Q13: Count the number of unique invoices.
+SELECT COUNT(DISTINCT(InvoiceNo))
+FROM orders;
+
 -- Q14: Find products that appear in more than 100 invoices.
+SELECT StockCode, COUNT(DISTINCT(InvoiceNo)) 
+FROM orders
+GROUP BY StockCode HAVING COUNT(DISTINCT(InvoiceNo)) > 100
+ORDER BY StockCode DESC;
+
 -- Q15: List customers whose total purchased quantity exceeds 100.
+SELECT CustomerID, SUM(Quantity) AS QUANTITY
+FROM orders
+GROUP BY CustomerID HAVING SUM(Quantity) > 100
+
 -- Q16: Retrieve rows where Country is IN ('France', 'Germany', 'Netherlands').
+SELECT *
+FROM orders
+WHERE Country IN ('France','Germany','Netherlands')
+ORDER BY Country;
+
 -- Q17: Find the maximum and minimum UnitPrice.
+SELECT MAX(UnitPrice), MIN(UnitPrice)
+FROM orders;
+
 -- Q18: Select all rows where CustomerID is not NULL.
+SELECT *
+FROM orders
+WHERE CustomerID IS NOT NULL;
+
 -- Q19: Find customers who placed more than 10 orders.
+SELECT CustomerID, COUNT(DISTINCT(InvoiceNo))
+FROM orders
+GROUP BY CustomerID HAVING COUNT(DISTINCT(InvoiceNo))> 10;
+
 -- Q20: List the 10 most recent transactions.
+
 -- Q21: Select transactions where Country is 'Germany' OR 'Spain'.
+SELECT *
+FROM orders 
+WHERE Country = 'Germany' OR Country = 'Spain'
+ORDER BY Country;
+
 -- Q22: Find the total quantity sold per country.
+SELECT Country, SUM(Quantity) AS Quantity
+FROM orders
+GROUP BY Country
+ORDER BY Quantity DESC;
+
 -- Q23: Retrieve products where Description ends with 'BAG'.
+SELECT StockCode, Description
+FROM orders
+WHERE Description LIKE '%BAG';
+
 -- Q24: Display all rows where Quantity is greater than 10.
+SELECT *
+FROM orders
+WHERE Quantity > 10;
+
 -- Q25: Calculate the average UnitPrice.
+SELECT AVG(UnitPrice)
+FROM orders;
+
 -- Q26: Find how many unique products exist.
+SELECT DISTINCT(StockCode)
+FROM orders;
+
 -- Q27: Retrieve rows where StockCode starts with a letter.
+SELECT *
+FROM orders
+WHERE StockCode ~ '^[A-Za-z]';
+
 -- Q28: Count the number of transactions per country.
+SELECT Country, COUNT(DISTINCT(InvoiceNo)) AS Count_transact
+FROM orders
+GROUP BY Country
+ORDER BY  Count_transact DESC;
+
 -- Q29: Find orders from 'France' with Quantity greater than 5.
+SELECT *
+FROM orders
+WHERE Country = 'France' AND Quantity > 5;
+
 -- Q30: Show products that have sold more than 500 total units.
+SELECT StockCode, SUM(Quantity) AS total_units_sold
+FROM orders
+GROUP BY StockCode HAVING SUM(Quantity) >500;
+
 -- Q31: Retrieve customers who bought more than 3 different products.
+
 -- Q32: Find the total quantity of items sold.
 -- Q33: Sort all transactions by InvoiceDate ascending.
 -- Q34: List customers from 'Germany' using DISTINCT.
